fb1410d04ad52b3b213f0b3bbc98cfccca655d6e
vanessacsn20 <159178908+vanessacsn20@users.noreply.github.com>
2024-03-25 23:34:22 -0300

    Update py.py

--- py.py
+++ py.py
@@ -1,8 +1,8 @@
 a = True
 b = False
-resultado1 = a and b
-resultado2 = a or b
-resultado3 = not a
+resultado1 = a and b # FALSE
+resultado2 = a or b # TRUE
+resultado3 = not a # FALSE
 
 # A ordem de precedência dos operadores lógicos é: NOT , AND e por fim, OR.
 
