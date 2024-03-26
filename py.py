ba5ef53b66d9dffb59aefd5a18dad6ef794ace96
vanessacsn20 <159178908+vanessacsn20@users.noreply.github.com>
2024-03-25 23:32:00 -0300

    Update py.py

--- py.py
+++ py.py
@@ -3,11 +3,24 @@ b = False
 resultado1 = a and b
 resultado2 = a or b
 resultado3 = not a
-*\A ordem de precedência dos operadores lógicos é: NOT , AND e por fim, OR. *\ 
-c = 3
-c == 4
-c > 2
-c ! = 3
-d = 4
-c is d
-c is not d
+
+# A ordem de precedência dos operadores lógicos é: NOT , AND e por fim, OR.
+
+c = 3 
+c == 4 # FALSE
+c > 2 # TRUE
+c != 3 # FALSE
+d = 4 
+c is d # FALSE
+c is not d # TRUE
+
+# a ordem dos operadores podem mudar utilizando-se dos parenteses
+
+x = True
+y = False
+z = True
+teste1 = x or y and not z  # Essa expressão é lida pelo interpretador dessa forma: x or (y and (not z)) TRUE
+
+# Utilizando os parenteses podemos mudar o resultado da expressão lógica:
+
+teste2 = (x or y) and (not z) # FALSO
